5c5bc8af10c526f2f6cc3251dfb203e478900679
Brionna White <Brionnanwhite@gmail.com>
2016-08-22 15:29:37 -0700

    ui router

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 var MovieApp;
 (function (MovieApp) {
-    angular.module("app", []);
+    angular.module("app", ['ui.router']).config(function ($stateProvider, $locationProvider, $urlrRouterProvider) {
+        $stateProvider.state('page1', {
+            url: '/page1',
+            templateUrl: 'page1.html',
+            controller: MovieApp.Controllers.MovieController,
+            controllerAs: 'vm'
+        }).state('page2', {
+            url: '/page2',
+            templateUrl: 'page2.html',
+            controller: MovieApp.Controllers.MovieController,
+            controllerAs: 'vm'
+        });
+        $urlrRouterProvider.otherwise('/page1');
+    });
 })(MovieApp || (MovieApp = {}));
--- app.ts
+++ app.ts
@@ -1,4 +1,22 @@
 
     namespace MovieApp {
-      angular.module ("app" , [])
-}
+      angular.module ("app" , ['ui.router']).config((
+        $stateProvider: ng.ui.IStateProvider,
+        $locationProvider: ng.ILocationProvider,
+        $urlrRouterProvider: ng.ui.IUrlRouterProvider
+      ) => {
+        $stateProvider.state('page1', {
+          url: '/page1',
+          templateUrl:'page1.html',
+          controller: MovieApp.Controllers.MovieController,
+          controllerAs: 'vm'
+       }).state('page2', {
+         url: '/page2',
+         templateUrl:'page2.html',
+         controller: MovieApp.Controllers.MovieController,
+         controllerAs: 'vm'
+      })
+      $urlrRouterProvider.otherwise('/page1');
+      // $locationProvider.html5Mode(true);
+    })
+  }
